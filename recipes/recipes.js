a59e8585bd9c9534b3a62bb71a1faa73be4c74ea
jcc6611 <jcc6611@rit.edu>
2020-08-09 18:22:43 -0400

    Fixed the previous modal bugs. Just need to format the text inside it now.

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -66,80 +66,63 @@ function changeDisplay(url) {
       results.forEach((el) => {
         console.log(el.title);
         mealsEl.innerHTML += `
-        <div class="meal" id="meal-${el.id}">
-        <h2>${el.title}</h2>
-        <img src="${el.image}" alt="${el.title}"/>
-        <button data-modal-target="#modal" class="meal-info" id="${el.id}">Info</button>
-        </div>`;
+        <div class="meal" id="meal">
+            <h2>${el.title}</h2>
+            <img src="${el.image}" alt="${el.title}"/>
+            <button data-modal-target="#modal" class="meal-info" id="${el.id}">Info</button>
+        </div>
+
+      <div class="modal" id="modal-${el.id}">
+        <div class="modal-header">
+          <div class="modal-title">${el.title}</div>
+          <button data-close-button class="close-button" id="close-${el.id}">&times;</button>
+        </div>
+        <div class="modal-body">${el.summary}</div>
+      </div>
+      <div id="overlay"></div>
+      `;
       });
       mealsEl.addEventListener("click", getInfo, false);
     })
     .catch((e) => console.log(e));
 }
 
-/* Gets ID of individual recipe when Info 
+/* Displays recipe info in a modal when the Info 
    button is clicked
 */
 function getInfo(e) {
-  let mealID, clicked;
+  let clicked;
+  const overlay = document.getElementById("overlay");
   clicked = e.target;
   if (clicked !== e.currentTarget) {
+    const modal = document.getElementById(`modal-${clicked.id}`);
     if (clicked.className === "meal-info") {
-      mealID = clicked.id;
-      console.log(mealID);
-      searchByID(mealID);
+      openModal(modal, overlay);
+      document
+        .getElementById(`close-${clicked.id}`)
+        .addEventListener("click", closeModal, false);
     }
-
     e.stopPropagation();
   }
 }
 
-// Search for an individual recipe by ID
-function searchByID(mealID) {
-  let url, apiKey;
-  apiKey = config.APIKey;
-  url = `https://api.spoonacular.com/recipes/${mealID}/information?apiKey=${apiKey}&number=1&instructionsRequired=true&includeNutrition=true`;
-  displayModal(url, mealID);
+// Closes the individual modal
+function closeModal(e) {
+  let clicked, modalID;
+  clicked = e.target;
+  const overlay = document.getElementById("overlay");
+  modalID = clicked.id.replace("close-", "");
+  const modal = document.getElementById(`modal-${modalID}`);
+  if (clicked.className === "close-button") {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 }
 
-// Get info from API by ID and display it in a modal
-function displayModal(url, mealID) {
-  fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((result) => result.json())
-    .then((data) => {
-      let mealsEl, openModalButtons, closeModalButtons, overlay, modal;
-      mealsEl = document.getElementById(`meal-${mealID}`);
-      console.log(mealsEl);
-      console.log(data);
-      mealsEl.innerHTML += `
-      <div class="modal" id="modal">
-        <div class="modal-header">
-          <div class="title">${data.title}</div>
-          <button data-close-button class="close-button">&times;</button>
-        </div>
-        <div class="modal-body">${data.instructions}</div>
-      </div>
-      <div id="overlay"></div>
-      `;
-      openModalButtons = document.querySelectorAll("[data-model-target]");
-      closeModalButtons = document.querySelectorAll("[data-close-button]");
-      overlay = document.getElementById("overlay");
-      modal = document.getElementById("modal");
-      modal.classList.add("active");
-      overlay.classList.add("active");
-      closeModalButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-          const theModal = button.closest(".modal");
-          theModal.classList.remove("active");
-          overlay.classList.remove("active");
-        });
-      });
-    })
-    .catch((e) => console.log(e));
+// Opens the individual modal
+function openModal(modal, overlay) {
+  modal.classList.add("active");
+  overlay.classList.add("active");
 }
 
 // Search button event listener
