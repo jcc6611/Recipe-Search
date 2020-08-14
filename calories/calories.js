685d4cc31d82cbc5274f42569154254ef28ec11a
jcc6611 <jcc6611@rit.edu>
2020-08-13 23:56:42 -0400

    Added htmlSelectors to condense code

--- calories/calories.js
+++ calories/calories.js
@@ -1,29 +1,46 @@
 // author: Jason Cheung
 
-// Gets values from meal name and calorie fields
-
+// Immediately invoked function to avoid global variables
 let display = (function () {
-  let total;
+  let total, date, dateEntry, dataEntries, htmlSelectors;
 
+  // Total calories for this session
   total = 0;
 
+  // Date and date formatting to store
+  date = new Date();
+
+  dateEntry = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+  // Reusable selectors
+  htmlSelectors = {
+    entryName: document.getElementById("add-meal-name"),
+    entryValue: document.getElementById("add-meal-calories"),
+    entries: document.getElementById("entries"),
+    totalEl: document.getElementById("total"),
+  };
+
+  // To store in database
+  dataEntries = {
+    date: dateEntry,
+    calories: total,
+  };
+
+  // Get the value from input
   function getValue() {
-    let name, value, data;
-    name = document.getElementById("add-meal-name").value;
-    value = document.getElementById("add-meal-calories").value;
+    let data;
     data = {
-      mealName: name,
-      calories: value,
+      mealName: htmlSelectors.entryName.value,
+      calories: htmlSelectors.entryValue.value,
     };
-    document.getElementById("add-meal-name").value = "";
-    document.getElementById("add-meal-calories").value = "";
+    htmlSelectors.entryName.value = "";
+    htmlSelectors.entryValue.value = "";
     addToDisplay(data);
   }
 
+  // Adds all additonal entries to display
   function addToDisplay(data) {
-    let entriesEl, totalEl;
-    entriesEl = document.getElementById("entries");
-    entriesEl.innerHTML += `<div class="entry" id="entry">
+    htmlSelectors.entries.innerHTML += `<div class="entry" id="entry">
           <div class="entry-name">
             <h2>${data.mealName}</h2>
           </div>
@@ -32,11 +49,24 @@ let display = (function () {
             </div>
       </div>`;
     total += parseInt(data.calories);
-    totalEl = document.getElementById("total");
-    totalEl.innerHTML = "";
-    totalEl.innerHTML += `<h1 id="total">Total: ${total} calories</h1>`;
+    htmlSelectors.totalEl.innerHTML = "";
+    htmlSelectors.totalEl.innerHTML += `<h1 id="total">Total: ${total} calories</h1>`;
+    dataEntries.calories = total;
     console.log(data);
   }
+
+  /* Gets the total calories, stores in a datatype 
+     to be put into a database
+  */
+  function getTotal() {
+    console.log(dataEntries);
+    total = 0;
+    htmlSelectors.entries.innerHTML = "";
+    htmlSelectors.totalEl.innerHTML = `<h1 id="total">Total: ${total} calories</h1>`;
+  }
+
+  // Event listener for submit button
+  document.getElementById("submit-button").addEventListener("click", getTotal);
 
   // Event listener for add button
   document.getElementById("add-button").addEventListener("click", getValue);
