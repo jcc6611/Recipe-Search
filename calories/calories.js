0bbb87c950f8d46d4da2d189345f7ae53cb946aa
jcc6611 <jcc6611@rit.edu>
2020-08-13 23:21:18 -0400

    Calories: added input fields, css, and total

--- calories/calories.js
+++ calories/calories.js
@@ -1,17 +1,50 @@
 // author: Jason Cheung
 
-function getValue() {
-  let value;
-  value = document.getElementById("save-entry").value;
-  console.log(value);
-  document.getElementById("save-entry").value = "";
-}
+// Gets values from meal name and calorie fields
 
-document.getElementById("save-button").addEventListener("click", getValue);
+let display = (function () {
+  let total;
 
-//Event listener for enter key
-document.addEventListener("keypress", function (event) {
-  if (event.keyCode === 13 || event.which === 13) {
-    getValue();
+  total = 0;
+
+  function getValue() {
+    let name, value, data;
+    name = document.getElementById("add-meal-name").value;
+    value = document.getElementById("add-meal-calories").value;
+    data = {
+      mealName: name,
+      calories: value,
+    };
+    document.getElementById("add-meal-name").value = "";
+    document.getElementById("add-meal-calories").value = "";
+    addToDisplay(data);
   }
-});
+
+  function addToDisplay(data) {
+    let entriesEl, totalEl;
+    entriesEl = document.getElementById("entries");
+    entriesEl.innerHTML += `<div class="entry" id="entry">
+          <div class="entry-name">
+            <h2>${data.mealName}</h2>
+          </div>
+            <div class="entry-calories">
+              <h2>${data.calories} calories</h2>
+            </div>
+      </div>`;
+    total += parseInt(data.calories);
+    totalEl = document.getElementById("total");
+    totalEl.innerHTML = "";
+    totalEl.innerHTML += `<h1 id="total">Total: ${total} calories</h1>`;
+    console.log(data);
+  }
+
+  // Event listener for add button
+  document.getElementById("add-button").addEventListener("click", getValue);
+
+  //Event listener for enter key
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      getValue();
+    }
+  });
+})();
